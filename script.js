bf0e353949a4e5cc25e9fb575c789d7a17430e8d
Holly Ashall <hollyashall@icloud.com>
2022-07-08 15:12:07 +0100

    started pseudo code

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+// show questions in chronological order (will get harder?)
+
+//show first question with related picture
+
+// display 3 different answers for each question
+
+// when the correct answer is pressed confetti cannon will work
+//when incorrect anser is pressed will go red and reset game??
+// there will not be a time pressure 
+//if reset is pressed will take user back to the start
+
+
+
+//FIRST SECTION: CHOOSE THE QUESTIONS
+ // put the questions and answers in
