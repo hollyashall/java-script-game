82305716210e162f29b5c1a7c3ffe9ba58a08404
Holly Ashall <hollyashall@icloud.com>
2022-07-12 14:29:17 +0100

    adding images to quiz

--- questions/questions.js
+++ questions/questions.js
@@ -1,43 +1,40 @@
 export const quizQuestions = [
-{
-  question:'What is the rarest M&M color?',
-  imgSrc: './images/M&M_spokescandies.jpeg',
-  solutions: 
-  [
-    { text: 'pink', isCorrect: false },
-    { text: 'yellow', isCorrect: false },
-    { text: 'brown', isCorrect: true },          
-]
-},
-{
-  question:'Which country invented ice cream?',
-  img:"",
-  solutions:[
-    { text: 'china', isCorrect: true }, 
-    { text: 'italy', isCorrect: false },
-    { text: 'USA', isCorrect: false },           
-]
-},
+  {
+    question: "What is the rarest M&M color?",
+    imgSrc: "./images/M&M_spokescandies.jpeg",
+    solutions: [
+      { text: "pink", isCorrect: false },
+      { text: "yellow", isCorrect: false },
+      { text: "brown", isCorrect: true },
+    ],
+  },
+  {
+    question: "Which country invented ice cream?",
+    imgSrc: "./images/pexels-teejay-1362534.jpg",
+    solutions: [
+      { text: "china", isCorrect: true },
+      { text: "italy", isCorrect: false },
+      { text: "USA", isCorrect: false },
+    ],
+  },
 
-{
-  question:'Which type of pasta’s name means “little worms”?',
-  img:"",
-  solutions:[
-    { text: 'spaghetti', isCorrect: false },
-    { text: 'fusilli', isCorrect: false },
-    { text: 'Vermicelli', isCorrect: true },          
-]
-},
+  {
+    question: "Which type of pasta’s name means “little worms”?",
+    imgSrc: "./images/pexels-lisa-1279330.jpg",
+    solutions: [
+      { text: "spaghetti", isCorrect: false },
+      { text: "fusilli", isCorrect: false },
+      { text: "Vermicelli", isCorrect: true },
+    ],
+  },
 
-{
-  question:'English Toffee Day is in which month?',
-  img:"",
-  solutions:[
-    { text: 'July', isCorrect: false },
-    { text: 'January', isCorrect: true }, 
-    { text: 'June', isCorrect: false },
-            
-]
-},
-
-]
+  {
+    question: "English Toffee Day is in which month?",
+    imgSrc: "./images/pexels-grooveland-designs-6627267.jpg",
+    solutions: [
+      { text: "July", isCorrect: false },
+      { text: "January", isCorrect: true },
+      { text: "June", isCorrect: false },
+    ],
+  },
+];
--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-import {quizQuestions} from "./questions/questions.js"
+import { quizQuestions } from "./questions/questions.js";
 console.log(quizQuestions);
 //GLOBAL VARIABLES
 const question = document.querySelector(".quiz__question");
@@ -7,65 +6,56 @@ const answers = document.querySelectorAll(".quiz__solutions");
 const answerA = document.querySelector(".quiz__solution-buttonA");
 const answerB = document.querySelector(".quiz__solution-buttonB");
 const answerC = document.querySelector(".quiz__solution-buttonC");
-const images =document.querySelector(".quiz__image")
+const images = document.querySelector(".quiz__image");
 const nextButton = document.querySelector(".button-next");
-const resetButton = document.querySelector(".button-reset")
-const container =document.querySelectorAll(".main-section")
-//^need to do this
+const resetButton = document.querySelector(".button-reset");
+const container = document.querySelectorAll(".main-section");
 
-
-
-// and an array to hold all of the questions that make up  quiz. 
+// and an array to hold all of the questions that make up  quiz.
 //Using an array will make the questions easy to iterate over:
 // each question is an object
 
 //FIRST SECTION: CHOOSE THE QUESTIONS: put in container
- // put the questions and answers in
+// put the questions and answers in
 
 // use object literals to represent the individual questions
 
-let questionNumber = 0;
+let questionNumber = 3;
+quizQuestions[questionNumber];
 
-//for each this????
-const questionContainer = () =>{
+nextButton.addEventListener("click", () => {
+  questionNumber = questionNumber++;
+  const result = questionContainer(questionNumber);
+});
 
-const quizInfo = quizQuestions[questionNumber];
+const questionContainer = () => {
+  console.log(questionNumber);
+  const quizInfo = quizQuestions[questionNumber];
 
-question.innerHTML = quizQuestions[0].question;
-console.log(question);
+  question.innerHTML = quizInfo.question;
+  console.log(question);
 
-images.innerHTML = quizQuestions[questionNumber].imgSrc[0];
+  console.log(images.src);
+  images.src = quizInfo.imgSrc;
 
-answerA.innerText = quizQuestions[questionNumber].solutions[0].text;
-answerB.innerText = quizQuestions[questionNumber].solutions[1].text;
-answerC.innerText = quizQuestions[questionNumber].solutions[2].text;
-}
+  answerA.innerText = quizInfo.solutions[0].text;
+  answerB.innerText = quizInfo.solutions[1].text;
+  answerC.innerText = quizInfo.solutions[2].text;
+};
 
 questionContainer();
 
 // event clicker which adds one to question number
 
-nextButton.addEventListener("click", () =>{
-  questionNumber = questionNumber++
-})
-
-
-// index get ecach next question 
-// change DOM with the question array 
+// index get ecach next question
+// change DOM with the question array
 
 //for each loop for the questions
 
+//Make a function to display the questions
 
+//submit, show the results
 
+// on click of the next button a new set of questions will appear
 
-
- //Make a function to display the questions
-
-
- //submit, show the results
-
- // on click of the next button a new set of questions will appear
-
-
-
- //End of quiz show score
+//End of quiz show score
