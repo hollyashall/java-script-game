63b80836afb7cb99a42b6411c0745a5df41c0758
Holly Ashall <hollyashall@icloud.com>
2022-07-12 16:06:57 +0100

    checking score

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { quizQuestions } from "./questions/questions.js";
 console.log(quizQuestions);
 //GLOBAL VARIABLES
 const question = document.querySelector(".quiz__question");
-const answers = document.querySelectorAll(".quiz__solutions");
+const answers = document.querySelectorAll(".quiz__solution-button");
 const answerA = document.querySelector(".quiz__solution-buttonA");
 const answerB = document.querySelector(".quiz__solution-buttonB");
 const answerC = document.querySelector(".quiz__solution-buttonC");
@@ -23,7 +23,7 @@ const quizContainer = document.querySelectorAll(".quiz");
 let questionNumber = 0;
 
 nextButton.addEventListener("click", () => {
-  questionNumber = questionNumber +1;
+  questionNumber = questionNumber + 1;
   quizInfo = quizQuestions[questionNumber];
   //re assigning quiz info based on question
   console.log(quizQuestions[questionNumber]);
@@ -45,12 +45,28 @@ const questionContainer = () => {
 
 questionContainer();
 
+//click the answers and see if write or wrong
 
+// when you click on correct answer get a message telling you correct or try again
 
+// if or else statement
 
+console.log(quizInfo.solutions[0].isCorrect);
+console.log(quizInfo.solutions[1].isCorrect);
+console.log(quizInfo.solutions[2].isCorrect);
 
+console.log(answers);
 
-//submit, show the results
+const checkSolutions = () => {
+  answers.addEventListener("click", () => {
+    if (console.log(quizInfo.solutions[0].isCorrect) == true) {
+      alert = "well done ";
+    } else if (console.log(quizInfo.solutions[0].isCorrect) != true) {
+      alert = "try again";
+    }
+  });
+};
+
 
 
 
