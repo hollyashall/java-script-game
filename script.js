ca23f5396ddcbc470e36c4d9f4cffa8c1e6603e3
Holly Ashall <hollyashall@icloud.com>
2022-07-12 15:22:03 +0100

    question changes on next question

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const answerC = document.querySelector(".quiz__solution-buttonC");
 const images = document.querySelector(".quiz__image");
 const nextButton = document.querySelector(".button-next");
 const resetButton = document.querySelector(".button-reset");
-const container = document.querySelectorAll(".main-section");
+const quizContainer = document.querySelectorAll(".quiz");
 
 // and an array to hold all of the questions that make up  quiz.
 //Using an array will make the questions easy to iterate over:
@@ -20,22 +20,22 @@ const container = document.querySelectorAll(".main-section");
 
 // use object literals to represent the individual questions
 
-let questionNumber = 3;
-quizQuestions[questionNumber];
+let questionNumber = 0;
 
 nextButton.addEventListener("click", () => {
-  questionNumber = questionNumber++;
-  const result = questionContainer(questionNumber);
+  questionNumber = questionNumber +1;
+  quizInfo = quizQuestions[questionNumber];
+  //re assigning quiz info based on question
+  console.log(quizQuestions[questionNumber]);
+  questionContainer();
 });
 
-const questionContainer = () => {
-  console.log(questionNumber);
-  const quizInfo = quizQuestions[questionNumber];
+let quizInfo = quizQuestions[questionNumber];
 
+const questionContainer = () => {
   question.innerHTML = quizInfo.question;
   console.log(question);
 
-  console.log(images.src);
   images.src = quizInfo.imgSrc;
 
   answerA.innerText = quizInfo.solutions[0].text;
@@ -45,17 +45,13 @@ const questionContainer = () => {
 
 questionContainer();
 
-// event clicker which adds one to question number
 
-// index get ecach next question
-// change DOM with the question array
 
-//for each loop for the questions
 
-//Make a function to display the questions
+
 
 //submit, show the results
 
-// on click of the next button a new set of questions will appear
+
 
 //End of quiz show score
